b2a451fa7cb90c2450f91e2fc45d8dff042c50b3
Albertino Neto <albeneto72@gmail.com>
2021-08-21 23:11:26 -0300

    Adicionado o body-parser

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.json())
 
 app.listen(3000, () => console.log('API esta rodando na porta 3000'))
