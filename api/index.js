7d1828d0218da6e370ed6e1bcc6f452dd6d782f0
Albertino Neto <albeneto72@gmail.com>
2021-08-29 10:36:45 -0300

    Classificando e centralizando tratamento de erros.

--- api/index.js
+++ api/index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const config = require('config')
 const roteador = require('./rotas/fornecedores')
+const NaoEncontrado = require('./erros/NaoEncontrado')
 
 const app = express()
 app.use(bodyParser.json())
 app.use('/api/fornecedores', roteador)
-
+app.use((error, req, res, next) => {
+    if(error instanceof NaoEncontrado) {
+        res.status(404);
+    }else{
+        res.status(400);
+    }
+    res.send(JSON.stringify({
+        mensagem: error.message,
+        id: error.idErro
+    }));
+})
 app.listen(config.get('api.port'), () => console.log('API esta rodando na porta 3000'))
