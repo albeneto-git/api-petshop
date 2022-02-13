5198a1ef42fb58e4de167991b0c555209b93243f
Albertino Neto <albeneto72@gmail.com>
2022-02-13 19:31:56 -0300

    Criação dos produtos de um fornecedor

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -10,7 +10,9 @@ module.exports = {
                 }
             }
         )
+        
+    },
+    inserir (dados){
+        return Modelo.create(dados)
     }
-
-
 }
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,12 +1,23 @@
 const roteador = require('express').Router({mergeParams: true})
 const Tabela = require('./TabelaProduto')
 const { json } = require('body-parser')
+const Produto = require('./Produto')
 
 roteador.get('/', async (req, res)=>{
     const produtos = await Tabela.listar(req.params.idFornecedor)
     res.send(
         JSON.stringify(produtos)
     );
+})
+
+roteador.post('/', async (req, res) => {
+    const idFornecedor = req.params.idFornecedor
+    const corpo        =  req.body
+    const dados        = Object.assign({}, corpo, { fornecedor : idFornecedor})    
+    const produto      = new Produto(dados)
+    await produto.criar()
+    res.status(201)
+    res.send(produto)
 })
 
 module.exports = roteador;
