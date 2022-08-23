6a0e40d9988f02356c21c3fc0987f12769a50777
Albertino Neto <albeneto72@gmail.com>
2022-08-23 18:42:45 -0300

    Versionando POST do fornecedor.

--- api/rotas/fornecedores/rotas.v2.js
+++ api/rotas/fornecedores/rotas.v2.js
@@ -1,9 +1,10 @@
 const roteador = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor');
 const SerializadorFornecedor = require('../../Serializador').SerializadorFornecedor;
+const Fornecedor = require('./Fornecedor')
 
 roteador.options('/', (req, res) =>{
-    res.set('Access-Control-Allow-Methods', 'GET')
+    res.set('Access-Control-Allow-Methods', 'GET, POST')
     res.set('Access-Control-Allow-Headers', 'Content-Type')
     res.status(204)
     res.end()
@@ -16,6 +17,21 @@ roteador.get('/', async (req, res) => {
         res.getHeader('Content-Type')
     );
     res.send(serializadorFornecedor.serializar(resultados));
+})
+
+roteador.post('/', async (req, res, next) =>{
+    try {
+        const dadosRecebidos = req.body;
+        const fornecedor = new Fornecedor(dadosRecebidos);
+        await fornecedor.criar();
+        res.status(201);
+        const serializadorFornecedor = new SerializadorFornecedor(
+            res.getHeader('Content-Type')
+        );
+        res.send(serializadorFornecedor.serializar(fornecedor));
+    } catch (error) {
+        next(error);
+    }
 })
 
 
